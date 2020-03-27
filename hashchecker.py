817049f9f640ea5b716f9f573f9aa36416ecdf17
Morgan Morel <morganmorel@bavc.org>
2020-03-26 23:53:34 -0700

    totally working. prints out a summary of successes and failures

--- hashchecker.py
+++ hashchecker.py
@@ -7,16 +7,71 @@
 ###simply_salesforce
 #######################################################################################################################
 
+import hashlib
 import os
-import sys
-import shutil
 import time
 import re
 import argparse
-import getpass
-import subprocess
 import config
 from simple_salesforce import Salesforce
+
+def makeFileList(sourceList,hashalg):
+    '''
+returns a list of file(s) to hashmove. list is a set of tuples with (filepath,hash). for now the hash is empty
+    '''
+    flist = []
+    for s in sourceList:
+        if os.path.isdir(s) is False: #if first argument is a file it's p easy
+            sourceFile = os.path.join(dirs, x)
+            sourceBasename = os.path.basename(s)
+            if sourceBasename.endswith(hashalg):
+                fileDict = {"Filepath": sourceFile,"Filename" : sourceBasename,"Barcode" : "","SFHash" : "","SidecarHash" : "", "Result": ""}
+                flist.append(dict(fileDict))
+            else:
+                print("WARNING: Input file " + sourceBasename + " ignored because it's not a hash file")
+        #if the start object is a directory things get tricky
+        elif os.path.isdir(s) is True:
+            if not s.endswith("/"):
+                s = s + "/" #later, when we do some string subs, this keeps os.path.join() from breaking on a leading / I HATE HAVING TO DO THIS
+            for dirs, subdirs, files in os.walk(s): #walk recursively through the dirtree
+                for x in files: #ok, for all files rooted at start object
+                    sourceFile = os.path.join(dirs, x) #grab the start file full path
+                    sourceBasename = os.path.basename(sourceFile)
+                    if sourceBasename.endswith(hashalg): #look only for sidecar checksum files
+                        fileDict = {"Filepath": sourceFile,"Filename" : sourceBasename,"Barcode" : "","SFHash" : "","SidecarHash" : "","Result": ""}
+                        flist.append(dict(fileDict))
+
+        else:
+            print("Critical Error. Could not determine if the input is a file or directory. Something is very wrong.")
+            sys.exit()
+    return flist
+
+def processList(sf,dictList,hashlength):
+
+    for dict in dictList:
+        dict = getBarcode(dict)
+        dict["SFHash"] = getChecksumFromRecord(querySF(sf,dict["Barcode"]))
+        dict["SidecarHash"] = readHash(dict.get("Filepath"), hashlength) #get the checksum from the sidecar file
+        if dict["SFHash"] == dict["SidecarHash"]:
+            dict["Result"] = True
+        else:
+            dict["Result"] = False
+    return dictList
+
+def getBarcode(dict):
+    barcode = dict.get("Filename")[4:11] #get the barcode from the filename
+    for b in barcode:
+        if not b.isdigit(): #this makes sure that the barcode is 7 numbers. if not it'll throw a failure
+            print("ERROR: File Barcode Not Found for " + sourceBasename)
+        else:
+            dict["Barcode"] = barcode
+    return dict
+
+
+def readHash(hashFile, hashlength):
+    with open(hashFile,'r') as f: #open it
+        storedHash = re.search('\w{'+hashlength+'}',f.read()).group() #get the hash
+    return storedHash
 
 def querySF(sf,barcode):
     result = sf.query("SELECT messageDigest__c FROM Preservation_Object__c WHERE Name = '" + barcode + "'")
@@ -25,6 +80,30 @@ def querySF(sf,barcode):
 def getChecksumFromRecord(sfData):
     checksum = sfData["records"][0].get("messageDigest__c")
     return checksum
+
+def processResults(dictList):
+    count = 0
+    success = 0
+    fail = 0
+    failList = []
+
+    for dict in dictList:
+        if dict["Result"]:
+            success += 1
+        else:
+            failList.append(dict["Filename"])
+            fail += 1
+        count += 1
+    print("\n")
+    print("Number of Checksums Processed: " + str(count))
+    print("Number of Successes: " + str(success))
+    print("Number of Failures: " + str(fail))
+    if fail > 0:
+        print("\n")
+        print("List of Failed Files:")
+        for f in failList:
+            print(f)
+    print("\n")
 
 def initLog(sourceList,destination,hashalg):
     '''
@@ -52,12 +131,8 @@ def make_args():
     initialize arguments from the cli
     '''
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d','--delete',action='store_true',dest='c',default=False,help="delete source files after copying")
-    parser.add_argument('-q','--quiet',action='store_true',dest='q',default=False,help="quiet mode, don't print anything to console")
     parser.add_argument('-a','--algorithm',action='store',dest='a',default='md5',choices=['md5','sha1','sha256','sha512'],help="the hashing algorithm to use")
-    parser.add_argument('sourceAndDestObj',nargs='+',help="the file or directory to hash/ move/ copy/ verify/ delete")
-    parser.add_argument('-xr','--bottomFolder',action='store_true',dest='xr',default=False,help="eXclude Root mode. This will move the folders and directories in the selecte input folders directly in the output, rather than the standard rsync style nesting")
-    #parser.add_argument('destinationDir',nargs='?',default=os.getcwd(),help="the destination parent directory")
+    parser.add_argument('sourceObj',nargs='+',help="As many files are directories you would like processed. Only sidecar checksum files are processed.")
     return parser.parse_args()
 
 def main():
@@ -65,21 +140,32 @@ def main():
     do the thing
     '''
     #init args from cli
-    #args = make_args()
-    ###INIT VARS###
+    args = make_args()
+
+    #init salesforce login#
+    sf = Salesforce(username=config.username,password=config.password,security_token=config.security_token)
 
     #Initialize log
     #initLog(sourceList,destinationDir,args.a)
 
-    sf = Salesforce(
-    username=config.username,
-    password=config.password,
-    security_token=config.security_token
-    )
+    #init variables
+    dictList = []
+    hashAlgorithm = hashlib.new(args.a) #creates a hashlib object that is the algorithm we're using
+    hashlengths = {'md5':'32','sha1':'40','sha256':'64','sha512':'128'}
+    hashlength = hashlengths[args.a] #set value for comparison later
 
-    #Query SalesForce and parse the data
-    sfData = querySF(sf,"1007008")
-    print(getChecksumFromRecord(sfData))
+    #Check that input conforms
+    if len(args.sourceObj) < 1: #if less than two input arguments we have to exit
+        print("CRITICAL ERROR: You must give this script at least one argument")
+        sys.exit()
 
+    #create list of dictionarie (which represent hash files) to be processed
+    dictList = makeFileList(args.sourceObj,args.a)
+
+    #process the list
+    dictList = processList(sf,dictList,hashlength)
+
+    #tally up the success and failures, print the failed files.
+    processResults(dictList)
 
 main()
